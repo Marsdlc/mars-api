4d628ea096fb9b6fff6ff58ba6ed2a51550991f9
mars <marsss816@gmail.com>
2024-04-16 16:06:12 +0800

    初始化

---
+++ api-backend/src/main/java/com/mars/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.mars.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 后端主启动类
+ *
+ * @author mars
+ */
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
